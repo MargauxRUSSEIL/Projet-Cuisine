f65fa5475e5ccf5ecce10910f3064936020be850
Sellin <marcsellin2016@gmai.com>
2020-11-23 22:05:13 +0100

    add Front Home

--- symfony/assets/app.js
+++ symfony/assets/app.js
@@ -15,4 +15,30 @@ import './styles/app.scss';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('ok')).offset().top}, 500, 'linear');
+  });
+});
+
+$( window ).on( "load", function() {
+    mainNav();
+    $(window).on('scroll', function() {
+        mainNav();
+    });
+    function mainNav() {
+        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        if (top > 40) $('.navbar-fixed-top').stop().animate({
+            "opacity": '1',
+            "top": '0'
+        });
+        else $('.navbar-fixed-top').stop().animate({
+            "top": '-70',
+            "opacity": '0'
+        });
+
+  }
+  });
+
 console.log('Hello Webpack Encore! Edit me in assets/app.js');
