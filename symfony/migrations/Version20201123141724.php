0a7f014947e8ca9dbc8c3a03b2e12e1059cce3c3
Margaux <margaux.russeil.19@gmail.com>
2020-11-25 10:57:06 +0100

    add recette index

--- symfony/migrations/Version20201123141724.php
+++ symfony/migrations/Version20201123141724.php
@@ -21,13 +21,12 @@ final class Version20201123141724 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE rating (id INT AUTO_INCREMENT NOT NULL, note DOUBLE PRECISION DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE create_recette DROP rating_star');
+        
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE rating');
-        $this->addSql('ALTER TABLE create_recette ADD rating_star DOUBLE PRECISION DEFAULT NULL');
     }
 }
