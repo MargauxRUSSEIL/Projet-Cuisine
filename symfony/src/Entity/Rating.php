6b7d287f51ee2d094a6957a255870af6e959aca9
pelinski <thibault.pelinski10@gmail.com>
2020-12-07 11:27:29 +0100

    Fix Entity Rating

--- symfony/src/Entity/Rating.php
+++ symfony/src/Entity/Rating.php
@@ -10,27 +10,27 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Rating
 {
-  /**
-   * @ORM\Id
-   * @ORM\GeneratedValue
-   * @ORM\Column(type="integer")
-   */
-  private $id;
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
-  /**
-   * @ORM\Column(type="float", nullable=true)
-   */
-  private $note;
+    /**
+     * @ORM\Column(type="float", nullable=true)
+     */
+    private $note;
 
-  /**
-   * @ORM\ManyToOne(targetEntity=User::class, inversedBy="ratings")
-   */
-  private $user;
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="ratings")
+     */
+    private $user;
 
-  /**
-   * @ORM\ManyToOne(targetEntity=CreateRecette::class, inversedBy="ratings")
-   */
-  private $recette;
+    /**
+     * @ORM\ManyToOne(targetEntity=CreateRecette::class, inversedBy="ratings")
+     */
+    private $recette;
 
     /**
      * @ORM\Column(type="text", nullable=true)
@@ -42,34 +42,41 @@ class Rating
         return $this->id;
     }
 
-  public function getNote(): ?float
-  {
-    return $this->note;
-  }
+    public function getNote(): ?float
+    {
+        return $this->note;
+    }
 
-  public function setNote(?float $note): self
-  {
-    $this->note = $note;
+    public function setNote(?float $note): self
+    {
+        $this->note = $note;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getUser(): ?User
-  {
-    return $this->user;
-  }
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
 
-  public function setUser(?User $user): self
-  {
-    $this->user = $user;
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
-    return $this;
-  }
+        return $this;
+    }
 
-  public function getRecette(): ?CreateRecette
-  {
-    return $this->recette;
-  }
+    public function getRecette(): ?CreateRecette
+    {
+        return $this->recette;
+    }
+
+    public function setRecette(?CreateRecette $recette): self
+    {
+        $this->recette = $recette;
+
+        return $this;
+    }
 
     public function getCommentaire(): ?string
     {
